45a9e15ecfe0192acaf627f36404e4bbb0ef4766
degregory-kevin <76945892+degregory-kevin@users.noreply.github.com>
2021-04-06 17:31:11 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo />
+            <h1>À propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dicta aliquam inventore minima quisquam maxime, cum beatae quos praesentium consequatur in dolorem ipsam asperiores culpa quidem a harum, perferendis earum reiciendis ipsa voluptatem. Unde, culpa hic quis quam sapiente laborum magnam cumque ea molestias nemo, voluptatem facilis eius. Atque harum est quod minima enim nulla amet itaque quisquam rem aspernatur!
+            </p>
+            <br/>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex magni dolorum amet sed earum eos qui molestiae eum voluptate. Ipsam laudantium aliquam exercitationem dolorum, ut et eaque ab in suscipit, cum non quaerat nulla molestiae repellendus voluptates. Officia quia reiciendis alias placeat enim totam aperiam, quos illum non dignissimos, quasi vero? Debitis, quis rem? Laborum sit eveniet aut nihil deleniti voluptates, asperiores, harum ipsum magnam, autem est recusandae voluptate tempora? Ipsa ea, quidem eum nemo animi dolores iste aperiam voluptates quibusdam fugiat expedita delectus nisi nulla qui earum facilis. Illo architecto sint, eius tempora, provident sequi ea corporis numquam, saepe reprehenderit cupiditate sit a eaque? Non illo quod, obcaecati fuga, enim ad, earum laboriosam iusto consequatur laudantium perspiciatis ea neque voluptas quibusdam reprehenderit itaque quas optio? Hic, molestiae architecto nam, laboriosam commodi mollitia nisi autem doloribus, ipsum laborum natus aut.
+            </p>
+        </div>
+    );
+};
+
+export default About;
